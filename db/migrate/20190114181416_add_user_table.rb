78f9a37aa701431dff2a371cefea60c95582d9fd
LiamBorner <liamborner@gmail.com>
2019-01-14 20:55:35 +0000

    added devise and create user model

--- db/migrate/20190114181416_add_user_table.rb
+++ db/migrate/20190114181416_add_user_table.rb
@@ -1,10 +1,4 @@
 class AddUserTable < ActiveRecord::Migration[5.2]
   def change
-    change_table :users do |t|
-      t.string :username
-      t.string :firstname
-      t.string :lastname
-      t.string :bandname
-    end 
   end
 end
--- db/migrate/20190114182255_add_data_to_table.rb
+++ db/migrate/20190114182255_add_data_to_table.rb
@@ -1,5 +1,4 @@
 class AddDataToTable < ActiveRecord::Migration[5.2]
   def change
-    add_column :tables, :username, :string
   end
 end
