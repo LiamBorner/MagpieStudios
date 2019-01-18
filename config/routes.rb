690eff6c38f1a4c71d4c11049403c9192092e9d0
LiamBorner <liamborner@gmail.com>
2019-01-18 19:27:17 +0000

    user can edit all attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :rails
   resources :tracks
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
 
   root to: 'welcome#index'
-  
+
 end
---
+++ db/migrate/20190118190552_add_data_to_user_table.rb
@@ -0,0 +1,8 @@
+class AddDataToUserTable < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :band_name, :string
+    add_column :users, :phone_number, :string
+
+  end
+end
