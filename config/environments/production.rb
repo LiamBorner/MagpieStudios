6c7359d74d401b5804f0957391aa3c4ad37c1f4b
LiamBorner <liamborner@gmail.com>
2019-05-12 16:20:11 +0100

    update to email production envrionment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,17 @@ Rails.application.configure do
 
 
   #this will need to be Charlie's email
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://magpiestudios.herokuapp.com/'' }
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+ :user_name => ENV['SENDGRID_USERNAME'],
+ :password => ENV['SENDGRID_PASSWORD'],
+ :domain => 'heroku.com',
+ :address => 'smtp.sendgrid.net',
+ :port => 587,
+ :authentication => :plain,
+ :enable_starttls_auto => true
+ }
 
 end
