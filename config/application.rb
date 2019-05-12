07861f22b3f38f338eab0342cc6a3e9d49e0b2a0
LiamBorner <liamborner@gmail.com>
2019-05-12 14:52:02 +0100

    config heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
+# Rails.application.configure do
+#
+#
+# config.assets.initialize_on_precompile = false
+# end
 
 
 module MagpieStudios
