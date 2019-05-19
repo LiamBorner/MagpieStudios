9a1bc8380119312dc7d8ff30a83985f93bc94f25
LiamBorner <liamborner@gmail.com>
2019-05-19 14:28:47 +0100

    hero banner styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,5 @@
             families: ["Oswald:200,300,400,500,600,700", "Droid Sans:400,700", "Lato:100,100italic,300,300italic,400,400italic,700,700italic,900,900italic", "Roboto:300,regular,500"]
         }
     });
+
+    
