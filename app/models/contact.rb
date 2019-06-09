c6bd5ad7da24eb46e25fee03acdfd1b4369d1ff6
LiamBorner <liamborner@gmail.com>
2019-06-09 11:56:26 +0100

    added css refs to fix mobile nav...or at least attempting to

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
   def headers
     {
     :subject => "Contact Form",
-    :to => "magpiestudio09@gmail.com",
+    :to => "liamborner@gmail.com",
     :from => %("#{name}" <#{email}>)
   }
 end
